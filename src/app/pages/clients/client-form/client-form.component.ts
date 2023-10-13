649a58601c4a18598f3d096347c893a211acef31
Kevin Gonzalo Cruz Martinez <kevingonzalocruzmartinez@gmail.com>
2023-10-13 11:30:30 -0600

    Se añadio el componente de la pagina para el formulario del cliente

---
+++ src/app/pages/clients/client-form/client-form.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import {  Router } from '@angular/router';
+import { Cliente } from 'src/app/interfaces/cliente';
+import { ClientsService } from 'src/app/services/clients.service';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'client-form',
+  templateUrl: './client-form.component.html',
+  styleUrls: ['./client-form.component.scss']
+})
+export class ClientFormComponent {
+
+  public cliente: Cliente = new Cliente();
+  public titulo: string = "Crear cliente";
+  public errores: string[] = [];
+  public formError = false;
+
+  //Inyecciones
+  constructor(private clienteService: ClientsService,
+              private router: Router){}
+
+
+  create(): void {
+    this.formError = false; // Restablece el estado de error
+
+  if (!this.cliente.nombreDelCliente || !this.cliente.apellido_P || !this.cliente.apellido_M || !this.cliente.fecha_naci || !this.cliente.domicilio || !this.cliente.correo || !this.cliente.telefono) {
+    this.formError = true;
+    return; // No continúes si hay campos vacíos
+  }
+
+    this.clienteService.create(this.cliente).subscribe(
+      (jsonResponse) => {
+        this.router.navigate(['/clientes']);
+        Swal.fire(
+          'Cliente agregado',
+          `El cliente ${jsonResponse.cliente.nombreDelCliente} ${jsonResponse.cliente.apellido_P} ${jsonResponse.mensaje}`,
+          'success'
+        );
+      },
+      err => {
+        this.errores = err.error.errors as string[];
+        console.error('Error en el codigo backend '+ err.status);
+        console.error(err.error.errors);
+      }
+    );
+  }
+
+}
