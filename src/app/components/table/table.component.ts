d47a0250bb04c34449a05ca8b2bc73f67e9ecf1b
Kevin Gonzalo Cruz Martinez <kevingonzalocruzmartinez@gmail.com>
2023-11-29 22:35:37 -0600

    - Se agrego modifico la logica de eliminar

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, NgZone, OnInit, ViewChild } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { ContextMenu } from 'primeng/contextmenu';
 import { Table } from 'primeng/table';
@@ -34,7 +34,6 @@ export class TableComponent implements OnInit {
     rowData: any = '';
     display: boolean = false;
     @Input() buttonConfig: any;
-    clienteEliminado!: [];
 
     constructor(private router: Router,
                 private clientsService: ClientsService,
@@ -43,6 +42,8 @@ export class TableComponent implements OnInit {
                 private route: ActivatedRoute){}
 
     ngOnInit() {
+    this.clientsService.getClientes();
+
     const url = this.router.url;
     let formularioSeleccionado: string;
     if (url === '/lista-clientes') {
@@ -61,10 +62,10 @@ export class TableComponent implements OnInit {
                 label: 'Detalles',
                 icon: 'pi pi-user',
                 command: () => {
-                  this.details == null
-                        ? console.log(this.rowData)
-                        : this.details(this.rowData);
+                  const id = this.rowData.id;
+                  console.log(id)
                   this.mostrarDetalles();
+                  this.detail();
                 },
             },
             {
@@ -103,96 +104,114 @@ export class TableComponent implements OnInit {
     }
 
     mostrarDetalles() {
-      this.details == null
-                        ? console.log(this.rowData)
-                        : this.details(this.rowData);
       this.display = true;
+      const mostrar = true;
     }
 
     cargarPagina(){
       location.reload();
     }
 
-    eliminar(): void {
+    detail(): void {
       if (this.rowData && this.rowData.id) {
-        const idToDelete = this.rowData.id;
+        const id = this.rowData.id;
         const url = this.router.url;
         if (url === '/lista-clientes') {
-          this.deleteCliente(idToDelete);
+          // this.detailClient(id);
         } else if (url === '/lista-empleados') {
-          this.deleteEmpleado(idToDelete);
+          // this.dtailEmpleyee(id);
         } else {
-          this.deleteCita(idToDelete);
+          // this.detailQuote(id);
+        }
+      }
+    }
+
+    eliminar(): void {
+      if (this.rowData && this.rowData.id) {
+        const id = this.rowData.id;
+        const url = this.router.url;
+        if (url === '/lista-clientes') {
+          this.deleteCliente(id);
+        } else if (url === '/lista-empleados') {
+          this.deleteEmpleado(id);
+        } else {
+          this.deleteCita(id);
         }
       }
     }
     deleteCliente(id: number): void {
-      const confirmation = confirm('¿Estás seguro de que deseas eliminar este cliente?');
-      if (confirmation) {
-        this.clientsService.delete(id).subscribe({
-          next: (resp) => {
-            this.clientDelete = this.clientDelete.filter(cli => cli.id !== id);
             Swal.fire({
-              title: 'Cliente eliminado!',
-              text: 'El cliente ha sido eliminado exitosamente',
-              icon: 'success',
-            })
-          },
-          error: (err) => {
-            console.error("Error al eliminar cliente:", err);
-            Swal.fire(
-              'Error!',
-              'Ocurrió un error al eliminar el cliente. Por favor, cheque si tiene una cita pendiente.',
-              'error'
-            );
+              title: "Advertencia!!",
+              text: "Esta seguro que desea eliminar el cliente de la lista?",
+              icon: "warning",
+              showCancelButton: true,
+              confirmButtonColor: "#3085d6",
+              cancelButtonColor: "#d33",
+              confirmButtonText: "Si, eliminar!"
+                }).then((result) => {
+                if (result.isConfirmed) {
+                  this.clientsService.delete(id).subscribe({
+                    next: (resp) => {
+                      this.clientDelete = this.clientDelete.filter(cli => cli.id !== id);
+                  Swal.fire({
+                  title: "Eliminado!",
+                  text: "El cliente a sido eliminado!.",
+                  icon: "success"
+                });
+              }
+            });
           }
+          location.reload()
         });
       }
-    }
     deleteEmpleado(id: number): void {
-      const confirmation = confirm('¿Estás seguro de que deseas eliminar este cliente?');
-      if (confirmation) {
-        this.employeesService.delete(id).subscribe({
-          next: (resp) => {
-            this.employeeDelet = this.employeeDelet.filter(empl => empl.id !== id);
-            Swal.fire(
-              'Empleado eliminado!',
-              'El empleado ha sido eliminado exitosamente',
-              'success'
-            );
-          },
-          error: (err) => {
-            console.error("Error al eliminar empleado:", err);
-            Swal.fire(
-              'Error!',
-              'Ocurrió un error al eliminar el empleado. Por favor, cheque si tiene una cita pendiente.',
-              'error'
-            );
-          }
-        });
-      }
+      Swal.fire({
+        title: "Advertencia!!",
+        text: "Esta seguro que desea eliminar el empleado de la lista?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Si, eliminar!"
+          }).then((result) => {
+          if (result.isConfirmed) {
+            this.employeesService.delete(id).subscribe({
+              next: (resp) => {
+                this.employeeDelet = this.employeeDelet.filter(emplo => emplo.id !== id);
+            Swal.fire({
+            title: "Eliminado!",
+            text: "El empleado a sido eliminado!.",
+            icon: "success"
+          });
+        }
+      });
     }
+    location.reload();
+  });
+}
     deleteCita(id: number): void {
-      const confirmation = confirm('¿Estás seguro de que deseas eliminar este cliente?');
-      if (confirmation) {
-        this.quotesService.delete(id).subscribe({
-          next: (resp) => {
-            this.quoteDelet = this.quoteDelet.filter(quot => quot.id !== id);
-            Swal.fire(
-              'Cita cancelada!',
-              'El cita ha sido eliminado exitosamente',
-              'success'
-            );
-          },
-          error: (err) => {
-            console.error("Error al eliminar cita:", err);
-            Swal.fire(
-              'Error!',
-              'Ocurrió un error al eliminar la cita.',
-              'error'
-            );
-          }
-        });
-      }
+      Swal.fire({
+        title: "Advertencia!!",
+        text: "Esta seguro que desea eliminar la cita?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Si, eliminar!"
+          }).then((result) => {
+          if (result.isConfirmed) {
+            this.quotesService.delete(id).subscribe({
+              next: (resp) => {
+                this.quoteDelet = this.quoteDelet.filter(quot => quot.id !== id);
+            Swal.fire({
+            title: "Eliminado!",
+            text: "El cliente a sido eliminado!.",
+            icon: "success"
+          });
+        }
+      });
     }
+    location.reload();
+  });
+}
 }
